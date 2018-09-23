e5ac9bb3649c8ad962df55dd5da7c00205bb861f
Calvin <huynhck001@gmail.com>
2018-09-23 02:31:00 +0200

    removed unused code

--- src/ts/components/user.update.form.ts
+++ src/ts/components/user.update.form.ts
@@ -51,12 +51,6 @@ export default {
 				m("div",
 					m("div",
 						m("button.button[type=submit]", "Save"),
-						// m("a", {
-						// 	href: "/users/",
-						// 	oncreate: m.route.link,
-						// },
-						// 	m("button", { onclick: UserModel.delete(UserModel.current.username) }, "Delete"),
-						// ),
 					),
 				),
 			],
